f1f6a8100aa0848dae8df2ac1a84b47945530c68
gopigoppu <gopi.learner93@gmail.com>
2018-04-01 23:40:35 +0530

    client and server merged with express routing with angular

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
-
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/profiles', express.static(path.join(__dirname, 'profiles')));
 
 require('dotenv').config();
 
@@ -17,10 +22,29 @@ MongoClient.connect(process.env.DB_CONN, (err, db) => {
 });
 
 
-app.get('/contacts', (req, res) => {
+app.get('/api/contacts', (req, res) => {
   const contactsCollection = database.collection('contacts');
 
   contactsCollection.find({}).toArray((err, docs) => {
     return res.json(docs);
   });
+});
+
+app.get('*', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.post('/api/contacts', (req, res) => {
+  const user = req.body;
+  const contactsCollection = database.collection('contacts');
+
+  contactsCollection.insertOne(user, (err, r) => {
+    if (err) {
+      return res.status(500).json({ error: 'Error inserting new record.' });
+    }
+
+    console.log(r);
+    const newRecord = r.ops[0];
+    return res.status(201).json(newRecord);
+  })
 });
